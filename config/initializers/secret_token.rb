64c49de251cd697f4a255f0984e4e9eae9ea7c47
Raul Izquierdo <rirmxer@gmail.com>
2012-11-10 12:40:23 -0600

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitdraw::Application.config.secret_token = 'd44c5c57f879c2130fb3ecd9f0273d90d2334ed76cfd64aac896d0806972830c687f5db4dc38fbaaaa6de8a49080f2358fd5b6aa93be10aad9d537b5c2ea1102'
